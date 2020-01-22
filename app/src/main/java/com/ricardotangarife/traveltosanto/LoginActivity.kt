ebd36a2a00ea0f1967875307a2b8c99dd2992f16
RicardoTangarife <ricardo.tangarife.97@gmail.com>
2020-01-22 16:06:46 -0500

    Prueba carga de registro

--- app/src/main/java/com/ricardotangarife/traveltosanto/LoginActivity.kt
+++ app/src/main/java/com/ricardotangarife/traveltosanto/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.ricardotangarife.traveltosanto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tv_registro.setOnClickListener{
+            var intent = Intent(this, RegistroActivity::class.java)
+            startActivityForResult(intent, 1234)
+        }
+        
     }
+
+
 }
